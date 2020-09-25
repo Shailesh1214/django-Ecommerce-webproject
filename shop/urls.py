4c1a609d3ec9dfe28cd312b7e17ffaa74e3f00e2
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-09-25 20:21:24 +0530

    app pipline

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUS"),
+    path("tracker/", views.tracker, name="Tracking status"),
+    path("search/", views.search, name ="search"),
+    path("productview/", views.productview, name ="productview"),
+    path("checkout/", views.checkout, name = "checkout"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Index Shop")
+    return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("we are at about")
+
+def contact(request):
+    return HttpResponse("we are at contactUs")
+
+def tracker(request):
+    return HttpResponse("We are at Tracking status")
+
+def search(request):
+    return HttpResponse("we are at search")
+
+def productview(request):
+    return HttpResponse("we are at productview")
+
+def checkout(request):
+    return HttpResponse("we are at checkout")
+
+    
