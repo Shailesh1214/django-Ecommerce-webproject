f4d26902b95967898a6d0931c0ae799eb7950efc
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-10-01 16:29:04 +0530

    Product View

--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,12 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path("", views.index, name="ShopHome"),
     path("about/", views.about, name="AboutUs"),
-    path("contact/", views.contact, name="ContactUS"),
-    path("tracker/", views.tracker, name="Tracking status"),
-    path("search/", views.search, name ="search"),
-    path("productview/", views.productview, name ="productview"),
-    path("checkout/", views.checkout, name = "checkout"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
- 
