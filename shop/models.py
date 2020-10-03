7da684f277571776196f4790ba5949ae2ca55296
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-10-03 16:51:39 +0530

    Order model

--- shop/models.py
+++ shop/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length =50)
-    category = models.CharField(max_length=50, default="")
-    subcategory = models.CharField(max_length=50, default="")
-    price = models.IntegerField(default=0)
-    desc = models.CharField(max_length=500)
+    category = models.CharField(max_length = 50, default="")
+    subcategory = models.CharField(max_length = 50, default="")
+    price = models.IntegerField(default = 0)
+    desc = models.CharField(max_length = 500)
     pub_date = models.DateField()
     image = models.ImageField(upload_to="shop/images", default="")
 
@@ -16,15 +16,27 @@ class Product(models.Model):
 
 # Contact model
 class Contact(models.Model):
-    msg_id = models.AutoField(primary_key =True)
-    name = models.CharField(max_length =70)
-    email = models.CharField(max_length=70, default="")
-    phone = models.CharField(max_length=70, default="")
-    desc = models.IntegerField(default=0)
+    msg_id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 70)
+    email = models.CharField(max_length = 70, default="")
+    phone = models.CharField(max_length = 70, default="")
+    desc = models.IntegerField(default = 0)
     desc = models.CharField(max_length=500,default="")
-    
 
 # method overriding
     def __str__(self):
         return self.name
 
+class Order(models.Model):
+    order_id = models.AutoField(primary_key = True)
+    items_Json = models.CharField(max_length = 5000)
+    name = models.CharField(max_length = 90)
+    email = models.CharField(max_length = 111)
+    address = models.CharField(max_length = 111)
+    city = models.CharField(max_length = 111)
+    state = models.CharField(max_length = 111)
+    zip_code = models.CharField(max_length =111)
+    phone = models.CharField(max_length = 70, default="")
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product,Contact
+from .models import Product, Contact, Order
 from math import ceil
 
 # Create your views here.
@@ -49,5 +49,20 @@ def productView(request, myid):
     product=Product.objects.filter(id=myid)
     print(product)
     return render(request, "shop/prodView.html",{'product':product[0]})
+
 def checkout(request):
+    if request.method == "POST":
+        items_Json= request.POST.get('itemsJson','')
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        address = request.POST.get('address1','') + " " +  request.POST.get('address2','')
+        city = request.POST.get('city','')
+        state=request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code','')
+        phone  = request.POST.get('phone','')
+        order = Order(items_Json=items_Json, name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html',{'thank':thank,'id': id})
     return render(request, 'shop/checkout.html')
