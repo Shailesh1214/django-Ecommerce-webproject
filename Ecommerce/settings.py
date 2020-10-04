6343758158c230d936e87e43fdd1d724fff05654
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-10-04 21:49:16 +0530

    traker

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Ecommerce/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
