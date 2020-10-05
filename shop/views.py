488d739422b3329f50998df5f62f4ca236b13953
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-10-05 13:48:48 +0530

    Tracker-order details

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from .models import Product, Contact, Order, OrderUpdate
 from math import ceil
 import json
 
+
 # Create your views here.
 from django.http import HttpResponse
 
@@ -46,7 +47,7 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps(updates, default=str)
+                    response = json.dumps([updates, order[0].items_json], default=str)
                 return HttpResponse(response)
             else:
                 return HttpResponse('{}')
